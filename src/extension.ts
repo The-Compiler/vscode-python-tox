303f6940290c0cbbb0892b072bb066d610243a3b
Daniel Heinze <daheinze@microsoft.com>
2022-04-06 16:59:27 +0200

    feat: updated test discovery

--- src/extension.ts
+++ src/extension.ts
@@ -33,7 +33,7 @@ async function getToxEnvs(projDir: string) {
 async function safeGetToxEnvs(projDir: string) {
 	try {
 		return await getToxEnvs(projDir);
-	} catch (error) {
+	} catch (error: any) {
 		vscode.window.showErrorMessage(error.message);
 		return;
 	}
@@ -86,6 +86,97 @@ async function selectMultipleCommand() {
 }
 
 export function activate(context: vscode.ExtensionContext) {
+	const controller = vscode.tests.createTestController('toxTestController', 'Tox Testing');
+	context.subscriptions.push(controller);
+
+	controller.resolveHandler = async (test) => { 
+		if (!test) {
+			await discoverAllFilesInWorkspace();
+		} 
+		else {
+			await parseTestsInFileContents(test);
+		}
+	};
+	
+	// When text documents are open, parse tests in them.
+	vscode.workspace.onDidOpenTextDocument(parseTestsInDocument);
+	
+	// We could also listen to document changes to re-parse unsaved changes:
+	vscode.workspace.onDidChangeTextDocument(e => parseTestsInDocument(e.document));
+
+	/**
+	 * In this function, we'll get the file TestItem if we've already found it,
+	 * otherwise we'll create it with `canResolveChildren = true` to indicate it
+	 * can be passed to the `controller.resolveHandler` to gets its children.
+	 * @param uri	The uri of the file to get or create
+	 * @returns vscode.TestItem
+	 */
+	function getOrCreateFile(uri: vscode.Uri) 
+	{
+		const existing = controller.items.get(uri.toString());
+		if (existing) {
+			return existing;
+		}
+	
+		const file = controller.createTestItem(uri.toString(), uri.path.split('/').pop()!, uri);
+		controller.items.add(file);
+
+		file.canResolveChildren = true;
+		return file;
+	}
+
+	/**
+	 * Parses for tests in the document.
+	 * @param e	The provided document
+	 * @param filename	The name of the file to look for. Default = tox.ini
+	 */
+	function parseTestsInDocument(e: vscode.TextDocument, filename: string = 'tox.ini') {
+		if (e.uri.scheme === 'file' && e.uri.path.endsWith(filename)) {
+			parseTestsInFileContents(getOrCreateFile(e.uri), e.getText());
+		}
+	}
+
+	/**
+	 * Parses the file to fill in the test.children from the contents
+	 * @param file The file to parse
+	 * @param contents The contents of the file
+	 */
+	async function parseTestsInFileContents(file: vscode.TestItem, contents?: string) {
+		
+		// TODO: Make the new TestItem be under the existing one
+		const newTestItem = controller.createTestItem("cowsay", "cowsay", file.uri);
+
+		controller.items.add(newTestItem);
+	}
+
+	async function discoverAllFilesInWorkspace() {
+		if (!vscode.workspace.workspaceFolders) {
+			return []; // handle the case of no open folders
+		}
+		
+		return Promise.all(
+			vscode.workspace.workspaceFolders.map(async workspaceFolder => {
+				const pattern = new vscode.RelativePattern(workspaceFolder, 'tox.ini');
+				const watcher = vscode.workspace.createFileSystemWatcher(pattern);
+			
+				// When files are created, make sure there's a corresponding "file" node in the tree
+				watcher.onDidCreate(uri => getOrCreateFile(uri));
+				// When files change, re-parse them. Note that you could optimize this so
+				// that you only re-parse children that have been resolved in the past.
+				watcher.onDidChange(uri => parseTestsInFileContents(getOrCreateFile(uri)));
+				// And, finally, delete TestItems for removed files. This is simple, since
+				// we use the URI as the TestItem's ID.
+				watcher.onDidDelete(uri => controller.items.delete(uri.toString()));
+			
+				for (const file of await vscode.workspace.findFiles(pattern)) {
+					getOrCreateFile(file);
+				}
+			
+				return watcher;
+			})
+		);
+	}
+
 	context.subscriptions.push(
 		vscode.commands.registerCommand('python-tox.select', selectCommand),
 		vscode.commands.registerCommand('python-tox.selectMultiple', selectMultipleCommand)
