ed6e54d2dc7b65a5433a51d95903c5efe8d95277
Oliver Lintner <oliver.lintner@microsoft.com>
2022-04-21 09:53:21 +0200

    output all files in code cov report. unit test add

--- src/test/unit/utilityService.test.ts
+++ src/test/unit/utilityService.test.ts
@@ -1,6 +1,7 @@
 import { UtilityService } from '../../service/utilityService';
 import { instance, mock, verify } from 'ts-mockito';
 import { VsCodeEnv } from '../../abstraction/environment';
+import { throws } from 'assert';
 
 describe('UtilityService tests', () => { 
     it('checking correct Url is opened', () => { 
@@ -11,5 +12,19 @@ describe('UtilityService tests', () => {
         service.openExternalUrl(expectedUri);
 
         verify(mockedEnv.openExternal(expectedUri)).once();
+    });
+
+    it('constructor throws error if env object is invalid', () => {
+        let env: VsCodeEnv;
+
+        throws(() => new UtilityService(env), TypeError);
+    });
+
+    it('openExternalUrl throws error if url is invalid', () => {
+        let url: string;
+
+        const mockedEnv = mock<VsCodeEnv>();
+        const service = new UtilityService(mockedEnv);
+        throws(() => service.openExternalUrl(url), TypeError);
     });
 });
