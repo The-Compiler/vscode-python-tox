603327d9086cdbd04b83f755c3f64256e33534fa
Peter Kirchner <peter.kirchner@microsoft.com>
2022-04-25 13:41:24 +0200

    test: add test for inherited env vars

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -119,7 +119,7 @@ suite('Extension Test Suite', () => {
 		// A position which DOES reference a variable set by passenv.
 		// Properties line and character in Position are zero-based, VS Code UI is one-based.
 		const position1 = new vscode.Position(18, 13); // point to variable PWD in section testenv:single_values_01
-		const position2 = new vscode.Position(53, 15); // point to variable USER in section testenv:multiple_values_01
+		const position2 = new vscode.Position(57, 15); // point to variable USER in section testenv:multiple_values_01
 
 		// Act
 
@@ -157,8 +157,8 @@ suite('Extension Test Suite', () => {
 		// A position which DOES reference a variable set by setenv.
 		// Properties line and character in Position are zero-based, VS Code UI is one-based.
 		const position1 = new vscode.Position(19, 20);	// point to variable LOCALUI_OUTPUT_PATH in section testenv:single_values_01
-		const position2 = new vscode.Position(28, 20);	// point to variable LOCALUI_OUTPUT_PATH in section testenv:single_values_02
-		const position3 = new vscode.Position(54, 20);	// point to variable REGISTRY_USER in section testenv:multiple_values_01
+		const position2 = new vscode.Position(30, 20);	// point to variable LOCALUI_OUTPUT_PATH in section testenv:single_values_02
+		const position3 = new vscode.Position(58, 20);	// point to variable REGISTRY_USER in section testenv:multiple_values_01
 
 		// Act
 
@@ -193,10 +193,10 @@ suite('Extension Test Suite', () => {
 		const toxIniPath = getExampleToxIniPath("envvars");
 		const textDocument = await vscode.workspace.openTextDocument(toxIniPath);
 
-		// A position which DOES reference a variable set by passenv or setenv.
+		// A position which DOES reference a variable set by setenv.
 		// Properties line and character in Position are zero-based, VS Code UI is one-based.
-		const position1 = new vscode.Position(36, 20);	// point to variable FILE_ENV_VAR_02 in section testenv:file_reference
-		const position2 = new vscode.Position(57, 20);	// point to variable FILE_ENV_VAR_03 in section testenv:multiple_values_01
+		const position1 = new vscode.Position(40, 20);	// point to variable FILE_ENV_VAR_02 in section testenv:file_reference
+		const position2 = new vscode.Position(61, 20);	// point to variable FILE_ENV_VAR_03 in section testenv:multiple_values_01
 
 		// Act
 
@@ -227,12 +227,11 @@ suite('Extension Test Suite', () => {
 		const toxIniPath = getExampleToxIniPath("envvars");
 		const textDocument = await vscode.workspace.openTextDocument(toxIniPath);
 
-		// A position which DOES reference a variable set by passenv or setenv.
 		// Properties line and character in Position are zero-based, VS Code UI is one-based.
 		const position1 = new vscode.Position(16, 10);	// point somewhere into section testenv:single_values_01
-		const position2 = new vscode.Position(32, 10);	// point somewhere into section testenv:file_reference
+		const position2 = new vscode.Position(36, 10);	// point somewhere into section testenv:file_reference
 		const position3 = new vscode.Position(12, 15);	// point into in the middle of section name testenv:single_values_01
-		const position4 = new vscode.Position(56, 15);	// point into in the middle of section name testenv:multiple_values_01
+		const position4 = new vscode.Position(60, 15);	// point into in the middle of section name testenv:multiple_values_01
 		const position5 = new vscode.Position(100, 100);	// point outside of document
 
 		// Act
@@ -257,6 +256,42 @@ suite('Extension Test Suite', () => {
 		assert.equal(sectionName3, expectedSectionName3, `For the given position the expected section name is: '${expectedSectionName3}'.`);
 		assert.equal(sectionName4, expectedSectionName4, `For the given position the expected section name is: '${expectedSectionName4}'.`);
 		assert.equal(sectionName5, expectedSectionName5, `For the given position the expected section name is: '${expectedSectionName5}'.`);
+
+	});
+
+	test('get hover message on inherited env vars from testenv', async () => {
+		// Arrange
+		// A text document containing a sample tox.ini file.
+		const toxIniPath = getExampleToxIniPath("envvars");
+		const textDocument = await vscode.workspace.openTextDocument(toxIniPath);
+
+		// A position which DOES reference a variable set by passenv or setenv.
+		// Properties line and character in Position are zero-based, VS Code UI is one-based.
+		const position1 = new vscode.Position(21, 20);	// point to variable FILE_ENV_VAR_11 in section testenv:single_values_01
+		const position2 = new vscode.Position(32, 15);	// point to variable NAME in section testenv:single_values_02
+
+		// Act
+
+		const environmentVariablesService = new EnvironmentVariablesService();
+		const resultUpdate = environmentVariablesService.analyzeDocument(textDocument);
+
+		const hoverMessage1 = environmentVariablesService.generateHoverMessage(textDocument, position1);
+		const hoverMessage2 = environmentVariablesService.generateHoverMessage(textDocument, position2);
+
+		// Assert
+
+		assert.equal(resultUpdate, true, 'Environment variables in the sample tox.ini file should have been found.');
+		assert.notEqual(hoverMessage1, null);
+		assert.notEqual(hoverMessage2, null);
+
+		const expectedHoverMessage1 = constructExpectedHoverMessage("testenv", "FILE_ENV_VAR_11", "value_11");
+		assert.equal(hoverMessage1 && hoverMessage1[0].value, expectedHoverMessage1, `For the given position the expected hover message is: '${expectedHoverMessage1}'.`);
+
+		const testPassEnvName = "NAME";
+		const testPassEnvValue = process.env[testPassEnvName] ?? "";
+		const expectedHoverMessage2 = constructExpectedHoverMessage("testenv", testPassEnvName, testPassEnvValue);
+
+		assert.equal(hoverMessage2 && hoverMessage2[0].value, expectedHoverMessage2, `For the given position the expected hover message is: '${expectedHoverMessage2}'.`);
 
 	});
 
