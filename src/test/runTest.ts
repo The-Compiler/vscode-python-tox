87f26d55e7cafa09d42fcdabd020479770995c08
Florian Bruhin <me@the-compiler.org>
2022-07-07 14:43:31 +0200

    tests: Use temporary user data dir
    Seems like a bad idea for test runs to be based on each other... Also
    after fixing 46ecf1f27b9870dc04e685c1ae223edffb6e9028 we now get double
    VS Code windows every launch.

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -17,13 +17,19 @@ async function main() {
 		const exampleBasenames = await fs_promises.readdir(exampleRoot);
 		const examplePaths = exampleBasenames.map((dir) => path.resolve(exampleRoot, dir));
 
+		const tempUserDir = await fs_promises.mkdtemp(path.resolve(".vscode-test", "user-data-"));
+
 		// Download VS Code, unzip it and run the integration test
-		await runTests({
-			extensionDevelopmentPath,
-			extensionTestsPath,
-			// open a multi-folder workspace with all test example dirs
-			launchArgs: ['--disable-extensions'].concat(examplePaths)
-		});
+		try {
+			await runTests({
+				extensionDevelopmentPath,
+				extensionTestsPath,
+				// open a multi-folder workspace with all test example dirs
+				launchArgs: ['--disable-extensions', '--user-data-dir', tempUserDir].concat(examplePaths)
+			});
+		} finally {
+			await fs_promises.rm(tempUserDir, {recursive: true});
+		}
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
