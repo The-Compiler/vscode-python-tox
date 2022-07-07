6c8d9365c097ffe72fef14ec53a94a4477d202b2
Florian Bruhin <me@the-compiler.org>
2022-07-07 15:50:16 +0200

    tests: Use system-wide tmpdir
    So that .vscode-test does not need to exist yet.

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import * as os from 'os';
 import * as fs_promises from 'fs/promises';
 
 import { runTests } from '@vscode/test-electron';
@@ -16,7 +17,7 @@ async function main() {
 	const exampleBasenames = await fs_promises.readdir(exampleRoot);
 	const examplePaths = exampleBasenames.map((dir) => path.resolve(exampleRoot, dir));
 
-	const tempUserDir = await fs_promises.mkdtemp(path.resolve(".vscode-test", "user-data-"));
+	const tempUserDir = await fs_promises.mkdtemp(path.resolve(os.tmpdir(), "vscode-test-user-data-"));
 
 	try {
 		// Download VS Code, unzip it and run the integration test
